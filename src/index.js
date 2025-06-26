820da2a50304481e1cd7c62533bb659c1a021410
sinan-894 <mohammedsinan1011@gmail.com>
2025-06-27 02:07:08 +0530

    Create a function to genrate form to enter location

--- src/index.js
+++ src/index.js
@@ -20,3 +20,37 @@ function extractDataFromResponse(data){
     const TEMP = data.currentConditions.temp
     console.log(TEMP)
 }
+
+function createForm(){
+    const form = document.createElement('form');
+    form.classList.add('location-form');
+    const inputField = document.createElement('input')
+    inputField.type = 'text'
+    inputField.name = 'location'
+    inputField.classList.add('location-input');
+    const label = document.createElement('label');
+    label.textContent = 'location';
+    label.for = inputField.name;
+    const button = document.createElement('button')
+    button.textContent = 'search';
+    button.classList.add('search-button');
+
+    function onSearch(event){
+        event.preventDefault()
+        getWeatherData(inputField.value)
+    }
+
+    button.addEventListener('click',event=>onSearch(event))
+    button.addEventListener('keyup',(event)=>{
+        if(event.key =="Enter")onSearch()
+    })
+
+    form.appendChild(label)
+    form.appendChild(inputField)
+    form.appendChild(button)
+
+    return form
+
+}
+
+document.body.appendChild(createForm())
