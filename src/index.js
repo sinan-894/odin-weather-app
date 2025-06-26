2c61d96b7df280272b8110b2b3a9a068cba94938
sinan-894 <mohammedsinan1011@gmail.com>
2025-06-27 01:46:45 +0530

    Convert to async and await

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 
 
 async function getWeatherData(location){
-    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&key=CNEXBFJPH8DQF552XSPHKFV37&contentType=json`)
-    const data = await response.json()
-    console.log(data) 
+    try{
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&key=CNEXBFJPH8DQF552XSPHKFV37&contentType=json`)
+        const data = await response.json()
+        console.log(data)
+        extractDataFromResponse(data)
+    }
+    catch{
+        console.log('enter correct location')
+        console.log('eneterd location:',location)
+    } 
 }
 
-getWeatherData('london')
+getWeatherData('perinthalmanna')
+
+
+function extractDataFromResponse(data){
+    const TEMP = data.currentConditions.temp
+    console.log(TEMP)
+}
