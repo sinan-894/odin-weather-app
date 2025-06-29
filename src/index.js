09f3644e69d6ee5f8f93ab10fa536cbb699706a3
sinan-894 <mohammedsinan1011@gmail.com>
2025-06-30 02:50:48 +0530

    Uncomment try catch and allow error handling

--- src/index.js
+++ src/index.js
@@ -4,23 +4,18 @@ import { displayWeatherData } from "./display"
 
 
 async function getWeatherData(location){
-    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&key=CNEXBFJPH8DQF552XSPHKFV37&contentType=json`)
-    const data = await response.json()
-    console.log(data)
-    const extractData  = extractDataFromResponse(data)
-    displayData(extractData)
-    console.log(extractData)
-    // try{
-    //     const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&key=CNEXBFJPH8DQF552XSPHKFV37&contentType=json`)
-    //     const data = await response.json()
-    //     console.log(data)
-    //     const extractData  = extractDataFromResponse(data)
-    //     console.log(extractData)
-    // }
-    // catch{
-    //     console.log('enter correct location')
-    //     console.log('eneterd location:',location)
-    // } 
+    try{
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&key=CNEXBFJPH8DQF552XSPHKFV37&contentType=json`)
+        const data = await response.json()
+        console.log(data)
+        const extractData  = extractDataFromResponse(data)
+        displayData(extractData)
+        console.log(extractData)
+    }
+    catch{
+        console.log('enter correct location')
+        console.log('eneterd location:',location)
+    } 
 }
 
 
