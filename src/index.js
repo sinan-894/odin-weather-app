7933b40c296739d95ce3a6537d585426df950ca8
sinan-894 <mohammedsinan1011@gmail.com>
2025-07-01 00:40:18 +0530

    Display Loading before data load

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ import searchSvg from "./search-alt-svgrepo-com.svg";
 
 async function getWeatherData(location){
     try{
+        console.log('loading......')
+        displayLoading()
         const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&key=CNEXBFJPH8DQF552XSPHKFV37&contentType=json`)
         const data = await response.json()
         console.log(data)
@@ -102,6 +104,14 @@ function displayError(){
     errorContainer.classList.add('error-div')
     errorContainer.textContent = "Sorry!Location Not Found,Please Enter Valid Location"
     dataDiv.appendChild(errorContainer)
+}
+
+function displayLoading(){
+    dataDiv.innerHTML = ""
+    const loadingContainer = document.createElement('div');
+    loadingContainer.classList.add('loading-div')
+    loadingContainer.textContent = "Loading....."
+    dataDiv.appendChild(loadingContainer)
 }
 
 const heading  = document.createElement('h1');
